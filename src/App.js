98dd0cea93bf4fc858aadd8d14fb010ac5bd77c8
Jhonnatan Urueña <jhonnatan.uruea@SINCO.NET>
2019-04-23 18:18:48 -0500

    pagina para gradientes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+//https://uigradients.com/#BackToEarth
+
 function App() {
   return (
     <div className="App">
@@ -10,17 +12,12 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>jau</p>
+       
       </header>
     </div>
   );
 }
+
 
 export default App;
